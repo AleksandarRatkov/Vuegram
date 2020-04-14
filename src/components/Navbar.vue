b4cc14793674c7a24c05df7a35cdd9cb66eab6ce
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-04-14 08:41:47 +0200

    Add new components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+<v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+    <v-app-bar-nav-icon @click.native="setDrawer(!drawer)" />
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">Welcome to Vuegram</span>
+    </v-toolbar-title>
+    <v-btn icon>
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
+    <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search" class="hidden-sm-and-down" />
+    <v-spacer />
+    <v-btn icon>
+        <v-avatar size="32px" item>
+            <v-img :src="require('../assets/aleksa.jpeg')" alt="logoImage" />
+        </v-avatar>
+    </v-btn>
+    
+    <v-btn icon large>
+        <v-avatar size="32px" item>
+            <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" />
+        </v-avatar>
+    </v-btn>
+</v-app-bar>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+    name: 'Navbar',
+    computed: {
+        ...mapState(['drawer'])
+    },
+    methods: {
+        ...mapMutations({
+            setDrawer: 'setDrawer'
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SideDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+<v-navigation-drawer :value="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+    <v-list dense>
+        <template v-for="item in items">
+            <v-row v-if="item.heading" :key="item.heading" align="center">
+                <v-col cols="6">
+                    <v-subheader v-if="item.heading">
+                        {{ item.heading }}
+                    </v-subheader>
+                </v-col>
+                <v-col cols="6" class="text-center">
+                    <a href="#!" class="body-2 black--text">EDIT</a>
+                </v-col>
+            </v-row>
+    
+            <v-list-item :key="item.text" link>
+                <v-list-item-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <router-link tag="div" :to="item.route"> {{ item.text }} </router-link>
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </template>
+    </v-list>
+</v-navigation-drawer>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'SideDrawer',
+    data() {
+        return {
+            items: [{
+                    icon: 'mdi-account-cog',
+                    text: 'Settings',
+                    route: '/settings'
+                },
+                {
+                    icon: 'mdi-view-dashboard',
+                    text: 'Dashboard',
+                    route: '/dashboard'
+                },
+                {
+                    icon: 'mdi-logout-variant',
+                    text: 'Logout',
+                    route: '/logout'
+                }
+            ],
+        }
+    },
+    computed: {
+        ...mapState(['drawer'])
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
