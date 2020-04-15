d4bef66e77b977200cc64568f41f0239e02b8084
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-04-15 20:01:30 +0200

    Update settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,6 +3,9 @@
     <v-container grid-list-xl text-xs-center>
         <v-layout>
             <v-flex>
+                <v-alert v-model="showSuccess" transition="scale-transition" dismissible color="green" border="left" elevation="20" colored-border icon="mdi-checkbox-marked-circle-outline">
+                    You've successfully updated your settings!
+                </v-alert>
                 <v-card height="fix-content" elevation="18">
                     <v-list-item>
                         <v-list-item-content>
@@ -11,10 +14,10 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-card-text>
-                        <v-text-field v-model="name" :placeholder="userProfile.name" label="Name" name="name" prepend-icon="person" type="text" />
-                        <v-text-field v-model="title" :placeholder="userProfile.title" label="Title" name="title" prepend-icon="domain" type="text" />
+                        <v-text-field v-model="userProfile.name" label="Name" name="name" prepend-icon="person" type="text" />
+                        <v-text-field v-model="userProfile.title" :placeholder="userProfile.title" label="Title" name="title" prepend-icon="domain" type="text" />
 
-                        <v-btn class="white--text" min-width="100px" color="blue darken-3" :disabled="name ==='' && title === ''" @click="updateProfile">
+                        <v-btn class="white--text" min-width="100px" color="blue darken-3" :disabled="userProfile.name ==='' && userProfile.title === ''" @click="updateProfile">
                             Update Profile
                         </v-btn>
                     </v-card-text>
@@ -34,33 +37,24 @@ import {
 export default {
     data() {
         return {
-            name: "",
-            title: "",
             showSuccess: false
         };
     },
+    created () {
+        this.setIsLoginPage(false)
+    },
     computed: {
         ...mapState(["userProfile"]),
-    },
-    created() {
-        this.setIsLoginPage(false)
     },
     methods: {
         ...mapMutations(['setIsLoginPage']),
         updateProfile() {
             this.$store.dispatch("updateProfile", {
-                name: this.name !== "" ? this.name : this.userProfile.name,
-                title: this.title !== "" ? this.title : this.userProfile.title
+                name: this.userProfile.name,
+                title: this.userProfile.title
             });
 
-            this.name = "";
-            this.title = "";
-
             this.showSuccess = true;
-
-            setTimeout(() => {
-                this.showSuccess = false;
-            }, 2000);
         }
     }
 };
