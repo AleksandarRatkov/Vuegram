46edd0c5f7169d8d0526b1e0738debf9b2804c2e
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-04-30 20:00:41 +0200

    set env variables for firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import 'firebase/firestore'
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey:  process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
